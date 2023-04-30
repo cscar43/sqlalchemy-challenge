a425535611e9457805da273075657a4b02827e78
cscar43 <123521403+cscar43@users.noreply.github.com>
2023-04-30 12:20:34 -0400

    Delete test.py

--- Surfs_Up/test.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-
-engine = create_engine("sqlite:///hawaii.sqlite")
-conn = engine.connect()
-
-# Query all records from the "measurement" table
-result = conn.execute("SELECT * FROM measurement")
-for row in result:
-    print(row)
